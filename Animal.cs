283a9991539d41d1030b8530aa783368cbd1b1ed
wille <144245981+WillePersson@users.noreply.github.com>
2023-10-16 12:34:51 +0200

    Add project files.

---
+++ Animal.cs
@@ -0,0 +1,134 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOPArv
+{
+    class Animal //baseclass
+    {
+        //Fields
+        public string _name;
+        public string _color;
+        public int _legs;
+        public int _weight;
+        public int _age;
+
+        public Animal(string name, string color, int legs, int weight, int age)//constructor
+        {
+            _name = name;
+            _color = color;
+            _legs = legs;
+            _weight = weight;
+            _age = age;
+        }
+        public void tryEscape() //Method
+        {
+            Console.WriteLine("Djuret försöker fly, det gick inte bra");
+        }
+        public void sleep() //Method
+        {
+            Console.WriteLine("Djuret sover");
+        }
+        public void awake() //Method
+        {
+            Console.WriteLine("Djuret vaknade");
+        }
+        public virtual void makeSound() //method
+        {
+            Console.WriteLine("Djuret låter jättehögt");
+        }
+    }
+    class Elephant : Animal //inheritance class from Animal
+    {
+        //Fields
+        public int _horns;
+
+        public Elephant(string name = "elefanten Dumbo", string color = "grå", int legs = 4,//cosntructor
+        int weight = 500, int age = 20, int horns = 2) : base(name, color, legs, weight, age)
+        {
+            _horns = horns;
+        }
+
+        public virtual void waveTrunk() //Method
+        {
+            Console.WriteLine("Elefanten svingar på snabbeln");
+        }
+        public override void makeSound() //overiding method
+        {
+            Console.WriteLine("Elefanten låter genom snabbeln");
+        }
+    }
+    class Lion : Animal //inheritance class from Animal
+    {
+        //Fields
+        public int _teeth;
+
+        public Lion(string name = "Lejonet Tyr", string color = "Orange/bersh", int legs = 4,//constructor
+            int weight = 60, int age = 7, int teeth = 22) : base(name, color, legs, weight, age)
+        {
+            _teeth = teeth;
+        }
+
+        public void Hungry() //Methods
+        {
+            Console.WriteLine("Lejonet är hungrig");
+        }
+        public override void makeSound() //overiding method
+        {
+            Console.WriteLine("Lejonet ryter");
+        }
+    }
+    class Zebra : Animal //inheritance class from Animal
+    {
+        //Fields
+        public int _blackStripes;
+
+        public Zebra(string name = "Zebran Doris", string color = "vit/svart", int legs = 4,
+            int weight = 40, int age = 10, int blackStripes = 15) : base(name, color, legs, weight, age)
+        {
+            _blackStripes = blackStripes;
+        }
+
+        public void hide() //Methods
+        {
+            Console.WriteLine("Zebran tror inte han/hon syns längre");
+        }
+        public override void makeSound() //overiding method
+        {
+            Console.WriteLine("Zebran är sjuk så den låter inte så mycket");
+        }
+
+    }
+    class AfricanSavannahElephant : Elephant //inheritance class from Elephant
+    {
+        public double _lenghtofTrunk = 1.5;//field
+
+        public override void waveTrunk() //Method
+        {
+            Console.WriteLine("Savana Elefanten svingar på snabbeln fort");
+        }
+        public override void makeSound() //Method
+        {
+            Console.WriteLine("Savana elefanten låter högt");
+        }
+
+
+    }
+    class AfricanForestElephant : Elephant //inheritance class from Elephant
+    {
+        public int _lenght = 4;//field
+
+        public override void waveTrunk() //Method
+        {
+            Console.WriteLine("Skogs Elefanten svingar på snabbeln sakta");
+        }
+        public override void makeSound() //Method
+        {
+            Console.WriteLine("Skogs elefanten låter inte så högt");
+        }
+
+    }
+}
+
---
+++ Program.cs
@@ -0,0 +1,54 @@
+﻿namespace OOPArv
+{           //wille persson - .NET23 -Labb 6
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Animal kängru = new Animal("Kängrun koko", "orange", 2, 35, 12);
+            Elephant elefant = new Elephant();
+            Lion lejon = new Lion();
+            Zebra zebra = new Zebra();
+            AfricanSavannahElephant savanaElefanten = new AfricanSavannahElephant();
+            AfricanForestElephant skogsElefanten = new AfricanForestElephant();
+
+            Console.WriteLine(kängru._name);
+            kängru.tryEscape();
+            kängru.makeSound();
+
+            Console.WriteLine();
+
+            Console.WriteLine(elefant._name);
+            Console.WriteLine(elefant._horns);
+            elefant.waveTrunk();
+            elefant.makeSound();
+
+            Console.WriteLine();
+
+            Console.WriteLine(lejon._name);
+            Console.WriteLine(lejon._teeth);
+            Console.WriteLine(lejon._weight);
+            lejon.Hungry();
+            lejon.makeSound();
+
+            Console.WriteLine();
+
+            Console.WriteLine(zebra._name);
+            zebra.hide();
+            Console.WriteLine(zebra._blackStripes);
+            Console.WriteLine(zebra._legs);
+            zebra.makeSound();
+
+            Console.WriteLine();
+
+            savanaElefanten.waveTrunk();
+            Console.WriteLine(savanaElefanten._lenghtofTrunk);
+            savanaElefanten.makeSound();
+
+            Console.WriteLine();
+
+            skogsElefanten.waveTrunk();
+            Console.WriteLine(skogsElefanten._lenght);
+            skogsElefanten.makeSound();
+        }
+    }
+}
